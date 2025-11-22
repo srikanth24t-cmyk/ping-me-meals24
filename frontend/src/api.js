11ab6a429cdbc50d24f171c8d4d3858559062e6d
srikanth24t-cmyk <srikanth24.t@gmail.com>
2025-11-22 22:16:37 +0530

    Delete frontend directory

--- frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-export async function createOrder(payload) {
-  const res = await fetch(process.env.REACT_APP_API_URL + '/order', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(payload)
-  });
-  return res.json();
-}
